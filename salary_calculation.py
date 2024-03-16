0cbbffcdd5643d85646dbfc601468bb7297adf47
p1gwowa <vladimirkostin98@gmail.com>
2024-03-16 14:54:24 +0100

    finished_without_comments

--- salary_calculation.py
+++ salary_calculation.py
@@ -1,14 +1,21 @@
 def total_salary(path) -> tuple:
     try:
-        with open("salary.txt", 'r') as file:
+        with open("salary_file.txt", 'r') as file:
             text = file.readlines()
             worker_salary = [el.strip() for el in text]
             name_salary = [el.split(',') for el in worker_salary]
-            return name_salary
-            # print(name_salary)
-            # print(type(name_salary))
+            name_salary_list = [el for element in name_salary for el in element]
+            salary_number_list = []
+            for el in name_salary_list:
+                try:
+                    salary_number = int(el)
+                    salary_number_list.append(salary_number)
+                except ValueError:
+                    pass
+            total = sum(salary_number_list)
+            average_salary = total//len(salary_number_list)
+        return print(f'Total sum of salary: {total}\nAverage salary: {average_salary}')
     except FileNotFoundError:
         return print('File not found')
 
-print(total_salary("path/to/salary.txt"))
-
+total_salary("path/to/salary.txt")
